a5437e28e099aac7b89a11ce22ca6199f4577186
Daniel C Farr <danielcfarr@gmail.com>
2019-02-05 20:11:24 -0700

    remove script versioning

--- wp-content/themes/tcf-theme/library/core.php
+++ wp-content/themes/tcf-theme/library/core.php
@@ -54,9 +54,9 @@ function tcf_head_cleanup() {
 	// WP version
 	remove_action( 'wp_head', 'wp_generator' );
 	// remove WP version from css
-	//add_filter( 'style_loader_src', 'tcf_remove_wp_ver_css_js', 9999 );
+	add_filter( 'style_loader_src', 'tcf_remove_wp_ver_css_js', 9999 );
 	// remove Wp version from scripts
-	//add_filter( 'script_loader_src', 'tcf_remove_wp_ver_css_js', 9999 );
+	add_filter( 'script_loader_src', 'tcf_remove_wp_ver_css_js', 9999 );
 
 }
 
