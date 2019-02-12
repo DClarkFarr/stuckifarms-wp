6f44cac803d403f69b05310782fbdcdefbeca8d7
Daniel C Farr <danielcfarr@gmail.com>
2019-02-11 18:22:00 -0700

    testimonials

--- wp-content/themes/tcf-theme/includes/testimonials-carousel.php
+++ wp-content/themes/tcf-theme/includes/testimonials-carousel.php
@@ -5,18 +5,19 @@
         </h2>
         <div class="section">
             <div class="owl-carousel testimonials-carousel">
+                
                 <div class="item">
                     <div class="d-flex justify-content-center align-items-center">
                         <div class="author text-center text-theme">
                             <div class="img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/SC-8-2 (1).jpg" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/testimonials/quinn-alan-gallup.jpg" alt="">
                             </div>
-                            <h5 class="font-weight-bold mb-0 mt-1">Mrs. Johnson</h5>
+                            <h5 class="font-weight-bold mb-0 mt-1">Quinn Alan Gallup</h5>
                         </div>
                         <div class="text bg-theme p-5 text-white">
-                            My brother just moved in and
-                            neighborhood is great.<br>
-                            Clean, quite and friendly Neighbors..
+                            This is a nice development with ponds, parks, and sidewalks in a small neighborhood. It is new real estate, close to Utah Hwy 7 interchange, groceries and schools within 5 miles.
+                            The open house is available M-Sat 10am to 6pm. 
+                            Retirement community and new family development.
                         </div>
                     </div>
                 </div>
@@ -24,14 +25,12 @@
                     <div class="d-flex justify-content-center align-items-center">
                         <div class="author text-center text-theme">
                             <div class="img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/SC-8-2 (1).jpg" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/testimonials/erika-rogers.jpg" alt="">
                             </div>
-                            <h5 class="font-weight-bold mb-0 mt-1">Mrs. Johnson</h5>
+                            <h5 class="font-weight-bold mb-0 mt-1">Erika Rogers</h5>
                         </div>
                         <div class="text bg-theme p-5 text-white">
-                            My brother just moved in and
-                            neighborhood is great.<br>
-                            Clean, quite and friendly Neighbors..
+                            Stucki Farms is truly a gorgeous community, lovely homes and fabulous amenities.
                         </div>
                     </div>
                 </div>
@@ -39,14 +38,38 @@
                     <div class="d-flex justify-content-center align-items-center">
                         <div class="author text-center text-theme">
                             <div class="img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/SC-8-2 (1).jpg" alt="">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/testimonials/mrzshadow.jpg" alt="">
                             </div>
-                            <h5 class="font-weight-bold mb-0 mt-1">Mrs. Johnson</h5>
+                            <h5 class="font-weight-bold mb-0 mt-1">MRZSHADOW</h5>
                         </div>
                         <div class="text bg-theme p-5 text-white">
-                            My brother just moved in and
-                            neighborhood is great.<br>
-                            Clean, quite and friendly Neighbors..
+                            My brother just moved in and neighborhood is great. Clean, quite and friendly Neighbors.
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="author text-center text-theme">
+                            <div class="img">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/testimonials/justin-madsen.jpg" alt="">
+                            </div>
+                            <h5 class="font-weight-bold mb-0 mt-1">Justin Madsen</h5>
+                        </div>
+                        <div class="text bg-theme p-5 text-white">
+                            My buddy lives out here. The huge field next to the houses is nice. Houses are also very nice. Wouldn't mind living here myself.
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="author text-center text-theme">
+                            <div class="img">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/testimonials/nicholas-ashworth.jpg" alt="">
+                            </div>
+                            <h5 class="font-weight-bold mb-0 mt-1">Nicholas Ashworth</h5>
+                        </div>
+                        <div class="text bg-theme p-5 text-white">
+                            It is shaping up to be one of the finer additions to saint george.
                         </div>
                     </div>
                 </div>
