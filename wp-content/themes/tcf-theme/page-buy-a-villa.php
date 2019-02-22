3ac98b113b3f4af599cb6edce9e83959bd2f3679
Daniel C Farr <danielcfarr@gmail.com>
2019-02-21 20:22:16 -0700

    shorten button text

--- wp-content/themes/tcf-theme/page-buy-a-villa.php
+++ wp-content/themes/tcf-theme/page-buy-a-villa.php
@@ -42,7 +42,7 @@ if (have_posts()){
                     <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/villa-rendering.jpg" class="img-fluid" alt="">
                     <div class="buttons text-right">
                         <a href="<?php the_permalink( get_field('vila_property_page', 'option') ); ?>" class="btn btn-theme btn-book">Details</a>
-                        <a href="<?php echo home_url('/contact/'); ?>" class="btn btn-book btn-theme m-btn-book mgrr-10">Buy a Villa Now</a>
+                        <a href="<?php echo home_url('/contact/'); ?>" class="btn btn-book btn-theme m-btn-book mgrr-10">Buy a Villa</a>
                     </div>
                 </div>
                 <p class="rbto-25 text-grey-f707 mt-5">
