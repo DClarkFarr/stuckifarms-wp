9561cf629bdc6e1204ca4930f05fca21eefcf7f9
Daniel C Farr <danielcfarr@gmail.com>
2019-02-23 13:13:12 -0700

    update villa thumb pic

--- wp-content/themes/tcf-theme/includes/rentals-villas.php
+++ wp-content/themes/tcf-theme/includes/rentals-villas.php
@@ -3,7 +3,7 @@
         <h2 class="page-heading">The Villas at the Cottages</h2>
         
         <div class="book-img no-shadow">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/villa-rendering.jpg" class="img-fluid" alt="">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/main-villa-thumb.jpg" class="img-fluid" alt="">
             <div class="buttons text-right">
                 <a href="<?php the_permalink( get_field('vila_property_page', 'option') ); ?>" class="btn btn-theme btn-book">Details</a>
                 <a href="<?php echo home_url('/vacation-rentals/'); ?>" class="btn btn-theme btn-book">Book Now</a>
