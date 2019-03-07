e47b7eb0979a8a6eae6c60e57e21e6003b5599c3
Daniel C Farr <danielcfarr@gmail.com>
2019-03-06 19:31:22 -0700

    manually place social icons

--- wp-content/themes/tcf-theme/single-post.php
+++ wp-content/themes/tcf-theme/single-post.php
@@ -11,46 +11,47 @@ page_title_block(get_the_title( $blogPage->ID ));
 			<div class="col-md-9">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
-              	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php 
-					$src = get_the_post_thumbnail_url(get_the_ID(), 'tcf-property-slide');
-					if($src){
-						echo "<div class='blog-img'>";
-							echo "<img class='img-fluid' src='". $src ."' alt='". get_the_title() ."'>";
-						echo "</div>";
-					}
-					?>
-                  	<h1><?php the_title(); ?></h1>
+          <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<?php 
+							$src = get_the_post_thumbnail_url(get_the_ID(), 'tcf-property-slide');
+							if($src){
+								echo "<div class='blog-img'>";
+									echo "<img class='img-fluid' src='". $src ."' alt='". get_the_title() ."'>";
+								echo "</div>";
+							}
+							?>
+                <h1><?php the_title(); ?></h1>
 
-                 	<p class="post-comments">
-						<?php printf('<i class="far fa-calendar"></i> %1$s <i class="far fa-user"></i> by %2$s <i class="far fa-comments"></i> %3$s',
-							/* the time the post was published */
-							'<span datetime="' . get_the_time('Y-m-d') . '">' . get_the_time(get_option('date_format')) . '</span>',
-							/* the author of the post */
-							'<span class="by">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
-							'<span class="commentsCount">'. get_comments_number('0', '1', '') .'</span>'
-						); ?>
-					</p>
+								<p class="post-comments">
+									<?php printf('<i class="far fa-calendar"></i> %1$s <i class="far fa-user"></i> by %2$s <i class="far fa-comments"></i> %3$s',
+										/* the time the post was published */
+										'<span datetime="' . get_the_time('Y-m-d') . '">' . get_the_time(get_option('date_format')) . '</span>',
+										/* the author of the post */
+										'<span class="by">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>',
+										'<span class="commentsCount">'. get_comments_number('0', '1', '') .'</span>'
+									); ?>
+								</p>
 
-                  	<?php the_content(); ?>
+                <?php the_content(); ?>
 
-                  	<?php 
-					$cats = explode('||', get_the_category_list('||'));
-					$cats = array_filter($cats, function($cat){
-						return stripos($cat, 'uncategorized') === false;
-					});
+                <?php 
+								$cats = explode('||', get_the_category_list('||'));
+								$cats = array_filter($cats, function($cat){
+									return stripos($cat, 'uncategorized') === false;
+								});
 
-					if($cats){
-					  printf( __( '<i class="far fa-tags"></i> Categories' ).': %1$s', implode(', ', $cats) ); 
-					}
+								if($cats){
+									printf( __( '<i class="far fa-tags"></i> Categories' ).': %1$s', implode(', ', $cats) ); 
+								}
 
-					?>
+								?>
 
-                  	<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:' ) . '</span> ', ', ', '</p>' ); ?>
+								<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:' ) . '</span> ', ', ', '</p>' ); ?>
 
-					<?php comments_template(); ?>
+								<?php comments_template(); ?>
 
-	            </div>
+								<?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
+	        </div>
 
 
 				<?php endwhile; ?>
